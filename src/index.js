502d351df94acbdc1a3c794ce87687c80b2b256d
Elijah <elijahmoye96@gmail.com>
2024-08-01 03:12:25 -0400

    Im so tired
    I got work tomorrow im in my head and i Start apple
    in two weeks Lords prayer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import _ from 'lodash';
 import '/Users/elijahmoye/Desktop/todo/Todo/src/css/style.css'
-import projectManager from '/Users/elijahmoye/Desktop/todo/Todo/src/java/project.js';
+import projectManager from './java/project';
 
+let addProject = document.getElementById('addProject')
 
-let addProjects = document.getElementById('addProject')
-
-addProjects.addEventListener('click', projectManager)
+addProject.addEventListener('click', projectManager)
--- src/java/project.js
+++ src/java/project.js
@@ -1,7 +1,26 @@
 export default function projectManager(){
 
-    let hello = document.getElementById('hello')
+    let formResults = document.getElementById('formResults')
+    let Close = document.getElementById('close')
+    let postProject = document.getElementById('postProject')
+    let projectSection = document.createElement('div')
 
-    hello.showModal();
+    formResults.showModal()
+
+
+    Close.addEventListener('click', () => {
+
+        let projectName = document.getElementById('Name-of-Project').value
+
+        projectSection.innerHTML = `${projectName}`
+
+        postProject.appendChild(projectSection)
+
+
+        formResults.close()
+
+    })
 
 }
+
+// Wow
