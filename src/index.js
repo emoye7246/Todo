10ad780e69301cfc2cb0d0aa1fd92592349c11d4
Elijah <elijahmoye96@gmail.com>
2024-08-10 13:04:00 -0400

    Fresh starting
    So im starting on a clean slate nothing else but
    the main focus is organization and simplicity

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import _ from 'lodash';
 import '/Users/elijahmoye/Desktop/todo/Todo/src/css/style.css'
-import { DialogControl } from './java/dialogs';
 import { dateControl } from './java/date';
-import {addDays} from "date-fns"
 
-
-let dialog = document.getElementById('formResults')
-let addProject = document.getElementById('addProject')
-let addTask = document.getElementById('addTask')
 let welcome = document.getElementById('Welcome')
 let today = document.getElementById('Today')
 
@@ -42,25 +36,7 @@ project.addEventListener('click', () => {
     postProject.style.display = 'flex'
 })
 
-addTask.addEventListener('click', () => {
 
-    dialog.showModal()
-
-    let userInput = new DialogControl
-    userInput.showTasks();
-
-
-})
-
-
-
-addProject.addEventListener('click', () => {
-    dialog.showModal();
-    
-    let userInput = new DialogControl
-    userInput.showProject()
-
-})
 
 
 // This goal now is to create the postTasks and PostProjects in javascript rather than in the dom so Great
