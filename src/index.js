2757b9ac5daa53d07adba68866e957f1c96ba9b3
Elijah <elijahmoye96@gmail.com>
2024-09-13 15:54:22 -0400

    Todo project is getting a revamp
    redoing this project because the last one was all
    over the place here we have more orginazation and
    its off to a good start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import _ from 'lodash';
 import '/Users/elijahmoye/Desktop/todo/Todo/src/css/style.css'
+import { TaskManager } from './java/taskManager';
+
+let addTask = document.getElementById('addTask')
+
+addTask.addEventListener('click', () => {
+
+    new TaskManager().getTaskInfo()
+})
+
 
--- src/java/taskManager.js
+++ src/java/taskManager.js
@@ -1,3 +1,7 @@
+let content = document.getElementById('content')
+
+
+
 export class TaskManager {
 
     constructor(titleInput, titleDescription, titleDate){
@@ -8,8 +12,91 @@ export class TaskManager {
 
     }
 
+    getTaskInfo(){
+
+        let userInput = document.getElementById('userInput')
+        userInput.innerHTML = ''
+    
+        userInput.showModal()
+    
+        let taskTitle = document.createElement('label')
+        taskTitle.htmlFor = 'taskTitle'
+        taskTitle.innerHTML = 'Task Title'
+    
+        let taskTitleInput = document.createElement('input')
+        taskTitleInput.id = 'taskTitle'
+    
+        let taskDescription = document.createElement('label')
+        taskDescription.innerHTML = 'Task Description'
+        taskDescription.htmlFor = 'taskDescription'
+    
+        let taskDescriptionInput = document.createElement('input')
+        taskDescriptionInput.id = 'taskDescription'
+    
+        let taskDate = document.createElement('label')
+        taskDate.innerHTML = 'Due Date'
+        taskDate.htmlFor = 'taskDate'
+    
+        let taskDateInput = document.createElement('input')
+        taskDateInput.type = 'date'
+        taskDateInput.id = 'taskDate'
+    
+    
+        let closeButton = document.createElement('button')
+        closeButton.innerHTML = 'Close'
+    
+        closeButton.addEventListener('click', () => {
+    
+           this.titleInput = taskTitleInput.value
+    
+           this.titleDescription = taskDescriptionInput.value
+    
+           this.titleDate = taskDateInput.value    
+    
+           this.postTask()
+    
+            userInput.close()
+        })
+        
+    
+        userInput.append(taskTitle, taskTitleInput, taskDescription, taskDescriptionInput, taskDate, taskDateInput, closeButton)
+    
+        
+        }
+
+
+    postTask(){
+                        
+
+            let userTitle = document.createElement('div')
+            userTitle.innerHTML = `Task Name : ${this.titleInput}`
+
+           
+            content.append(userTitle)
+
+            this.storeMyTask()
+
+    }
+
+    storeMyTask(){
+
+        let myTask = []
+
+        content.querySelectorAll('div').forEach(function(element){
+
+            myTask.push(element.innerHTML)
+
+            localStorage.setItem('task', JSON.stringify(myTask))
+
+
+        })
+
+        console.log(myTask)
 
     
 
- 
+        
+    }
+
 }
+// ok
